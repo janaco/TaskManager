8de679e6c68af290626795389e8030edc32e9bb7
Jana Kosar <yana.kosar@gmail.com>
2018-01-24 11:43:19 +0200

    Compress covers and database into .zip file

--- app/src/main/java/com/nandy/taskmanager/mvp/model/ZipModel.java
+++ app/src/main/java/com/nandy/taskmanager/mvp/model/ZipModel.java
@@ -27,69 +27,32 @@ public class ZipModel {
     private static final int BUFFER_SIZE = 2048;
     private static final String BACKUP_FILE_NAME = "backup.zip";
     private static final String BACKUP_DB_FILE = "backup.db";
-    private static final String DIRECTORY_COVERS = "covers";
+    private static final String IMAGE_EXTENSION = ".png";
+    private static final String DB_EXTENSION = ".db";
     private final Context mContext;
 
     public ZipModel(Context context) {
         mContext = context;
     }
 
-    public File zip() {
+    public File compress(File []files) {
 
-        File zipFile = new File(mContext.getFilesDir(), BACKUP_FILE_NAME);
-        try {
-            zipFile.createNewFile();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        byte data[] = new byte[BUFFER_SIZE];
+        File zipFile = createBackupZipFile(mContext.getFilesDir(), BACKUP_FILE_NAME);
+        byte buffer[] = new byte[BUFFER_SIZE];
 
         try {
             FileOutputStream fileOutputStream = new FileOutputStream(zipFile);
             ZipOutputStream zipOutputStream = new ZipOutputStream(new BufferedOutputStream(fileOutputStream));
-            BufferedInputStream bufferedInputStream;
 
-            for (File file : getFilesToZip()) {
+            for (File file : files) {
 
                 if (file.isDirectory()) {
-
-                    for (File f : file.listFiles()) {
-
-                        if (f.getPath().endsWith(".png")) {
-
-                            FileInputStream fileInputStream = new FileInputStream(f);
-                            bufferedInputStream = new BufferedInputStream(fileInputStream, BUFFER_SIZE);
-
-                            String fileName = f.getPath().substring(
-                                    f.getPath().lastIndexOf(File.separator) + 1);
-                            Log.d("ZIP_MODEL", "zip.entry: " + fileName);
-
-                            ZipEntry entry = new ZipEntry(fileName);
-                            zipOutputStream.putNextEntry(entry);
-                            int count;
-                            while ((count = bufferedInputStream.read(data, 0, BUFFER_SIZE)) != -1) {
-                                zipOutputStream.write(data, 0, count);
-                            }
-                            bufferedInputStream.close();
-                        }
-                    }
-                } else if (file.getPath().contains("tasks")){
-
-
-                    FileInputStream fileInputStream = new FileInputStream(file);
-                    bufferedInputStream = new BufferedInputStream(fileInputStream, BUFFER_SIZE);
-                    String fileName = file.getPath().substring(
-                            file.getPath().lastIndexOf(File.separator) + 1) + ".db";
-                    Log.d("ZIP_MODEL", "zip.entry: " + fileName);
-
-                    ZipEntry entry = new ZipEntry(fileName);
-                    zipOutputStream.putNextEntry(entry);
-                    int count;
-                    while ((count = bufferedInputStream.read(data, 0, BUFFER_SIZE)) != -1) {
-                        zipOutputStream.write(data, 0, count);
-                    }
-                    bufferedInputStream.close();
+                    checkDir(zipOutputStream, file, buffer);
+                } else if (file.getPath().endsWith(AppDatabase.DB_NAME)) {
+                    String fileName = getFileName(file).concat(DB_EXTENSION);
+                    compress(zipOutputStream, file, fileName, buffer);
+                } else if (file.getPath().endsWith(IMAGE_EXTENSION)) {
+                    compress(zipOutputStream, file, getFileName(file), buffer);
                 }
 
             }
@@ -102,18 +65,9 @@ public class ZipModel {
         return zipFile;
     }
 
-    private File[] getFilesToZip() {
-
-        return new File[]{
-                mContext.getFilesDir(),
-                mContext.getDatabasePath(AppDatabase.DB_NAME)};
-    }
-
-
     public void unzip() {
         FileInputStream inputStream = null;
         ZipInputStream zipInputStream = null;
-        OutputStream outputStream = null;
 
         try {
             inputStream = mContext.openFileInput(BACKUP_FILE_NAME);
@@ -121,29 +75,10 @@ public class ZipModel {
             ZipEntry zipEntry;
             while ((zipEntry = zipInputStream.getNextEntry()) != null) {
 
-                if (zipEntry.getName().contains(".db")) {
-                    if (!zipEntry.isDirectory()) {
-
-                        new File(mContext.getFilesDir(), BACKUP_DB_FILE);
-
-                        try {
-                            outputStream = mContext.openFileOutput(BACKUP_DB_FILE, Context.MODE_PRIVATE);
-
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                        }
-                        copyStream(zipInputStream, outputStream);
-                        zipInputStream.closeEntry();
-                        if (outputStream != null) {
-                            outputStream.close();
-                        }
-                        outputStream = null;
-
-
-                    }
-                } else {
-
-                    unzipMarkers(zipEntry, zipInputStream);
+                if (zipEntry.getName().contains(DB_EXTENSION)) {
+                    unzip(zipInputStream, BACKUP_DB_FILE);
+                } else if (!zipEntry.isDirectory()) {
+                    unzip(zipInputStream, zipEntry.getName());
                 }
             }
             zipInputStream.close();
@@ -152,30 +87,72 @@ public class ZipModel {
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
-            closeStreams(inputStream, zipInputStream, outputStream);
+            closeStreams(inputStream, zipInputStream);
         }
     }
 
-    private void unzipMarkers(
-            ZipEntry zipEntry,
-            ZipInputStream zipInputStream) throws IOException {
-        OutputStream outputStream;
+    private File createBackupZipFile(File parentDir, String fileName) {
+        File zipFile = new File(parentDir, fileName);
 
+        if (!zipFile.exists()) {
+            try {
+                zipFile.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
 
-        if (!zipEntry.isDirectory()) {
-            String name = zipEntry.getName();
-            Log.d("ZIP_MODEL", "unzip.image: " + name);
-                outputStream = mContext.openFileOutput(name, Context.MODE_PRIVATE);
+        return zipFile;
+    }
+
+    private void checkDir(ZipOutputStream zipOutputStream, File dir, byte[] buffer) throws IOException {
+
+        for (File file : dir.listFiles()) {
+
+            if (file.isDirectory()) {
+                checkDir(zipOutputStream, dir, buffer);
+            } else if (file.getPath().endsWith(IMAGE_EXTENSION)) {
+                compress(zipOutputStream, file, getFileName(file), buffer);
+            }
+        }
+    }
+
+    private void compress(ZipOutputStream zipOutputStream, File file, String fileName, byte[] buffer) throws IOException {
+        FileInputStream fileInputStream = new FileInputStream(file);
+        BufferedInputStream bufferedInputStream = new BufferedInputStream(fileInputStream, BUFFER_SIZE);
+
+        ZipEntry entry = new ZipEntry(fileName);
+        zipOutputStream.putNextEntry(entry);
+        int count;
+        while ((count = bufferedInputStream.read(buffer, 0, BUFFER_SIZE)) != -1) {
+            zipOutputStream.write(buffer, 0, count);
+        }
+        bufferedInputStream.close();
+    }
+
+    private String getFileName(File file) {
+        return file.getPath().substring(
+                file.getPath().lastIndexOf(File.separator) + 1);
+    }
+
+    private void unzip(ZipInputStream zipInputStream, String fileName) throws IOException {
+        OutputStream outputStream = null;
+        try {
+            outputStream = mContext.openFileOutput(fileName, Context.MODE_PRIVATE);
 
             copyStream(zipInputStream, outputStream);
             zipInputStream.closeEntry();
-            outputStream.close();
-            outputStream = null;
+            if (outputStream != null) {
+                outputStream.close();
+            }
+        } finally {
+            if (outputStream != null) {
+                outputStream.close();
+            }
         }
     }
 
-
-    private static void copyStream(InputStream inputStream, OutputStream outputStream) {
+    private void copyStream(InputStream inputStream, OutputStream outputStream) {
         byte[] buffer = new byte[BUFFER_SIZE];
         try {
             int read;
@@ -187,16 +164,8 @@ public class ZipModel {
         }
     }
 
-    private static void closeStreams(FileInputStream fileInputStream,
-                                     ZipInputStream zipInputStream,
-                                     OutputStream outputStream) {
-        if (outputStream != null) {
-            try {
-                outputStream.close();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
+    private void closeStreams(FileInputStream fileInputStream,
+                              ZipInputStream zipInputStream) {
         if (zipInputStream != null) {
             try {
                 zipInputStream.closeEntry();
